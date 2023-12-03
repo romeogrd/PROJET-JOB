398b27162b006af49d49e4395936a383d6963215
romeogrd <romeogerard2@hotmail.fr>
2023-12-03 17:00:41 +0100

    essai 15

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ app.use (express.urlencoded({ extended: true}));
 app.use(express.static('public')); //pour les img et le css
 app.use (express.json());
 
+require('dotenv').config();
+const viewsPath = process.env.VIEWS_PATH;
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 //db connection
-mongoose.connect ('mongodb+srv://romeogerard:Romeo44!@cluster0.shclbxx.mongodb.net/JobApplyTracker')
+mongoose.connect (process.env.MONGODB_URI)
 .then((result) => app.listen(3000))
 .then(console.log('Connect to the database'))
 .catch((err) => console.log(err));
