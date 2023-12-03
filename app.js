ddcecb11a1902a8558c8ac590a55787006b13950
romeogrd <romeogerard2@hotmail.fr>
2023-12-03 10:17:09 +0100

    version1
    authentification 1

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const app = express();
+
+app.use (express.json());
+app.use (express.urlencoded({ extended: true}));
+
+app.use(express.static('public')); //pour les img et le css
+
+app.set('view engine', 'ejs');
+
+//db connection
+mongoose.connect ('mongodb+srv://romeogerard:Romeo44!@cluster0.shclbxx.mongodb.net/JobApplyTracker')
+.then((result) => app.listen(3000))
+.then(console.log('Connect to the database'))
+.catch((err) => console.log(err));
+
+/* app.listen(3000, () => {
+        console.log('server is started');
+});
+ */
+app.get('/', (req, res)  => {
+    res.render('dashboard');
+}); 
+app.get('/dashboard', (req, res) => res.render('dashboard'));
+app.use(authRoutes);
