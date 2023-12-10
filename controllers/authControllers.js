0ab296498b951b2970992954b93be53e23e473ee
romeogrd <romeogerard2@hotmail.fr>
2023-12-10 11:56:57 +0100

    update

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -7,6 +7,16 @@ const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: ''};
 
+    //incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'that email is not registered';
+    }
+
+    //incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'that password is incorrect';
+    }
+
     //duplicate error code
     if(err.code === 11000) {
         errors.email ='That email is already registered';
@@ -56,8 +66,17 @@ module.exports.signup_post = async (req,res) => {
 
 module.exports.login_post = async (req,res) => {
     const { email, password } = req.body;
-    console.log (email, password);
-    res.send('user login');
+
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000});
+        res.status(200).json({user: user._id });
+    }
+    catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
 }
 
 module.exports.index_get = (req, res) => {
--- models/User.js
+++ models/User.js
@@ -25,11 +25,25 @@ userSchema.post('save', function(doc, next) {
 
 // fire a function before doc saved to db
 userSchema.pre('save', async function (next) {
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
 })
 
+
+// static method to login user
+
+userSchema.statics.login= async function(email, password) {
+    const user = await this.findOne({ email: email});
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth){
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email')
+}
 
 const User = mongoose.model('user', userSchema);
 
