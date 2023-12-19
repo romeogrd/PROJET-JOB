44bffffd75c3efd0b233e3e3d2839ec4a1221a27
romeogrd <romeogerard2@hotmail.fr>
2023-12-19 14:01:08 +0100

    bcrypt

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Job = require('./Job')
 const { isEmail } = require('validator');
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 
 
