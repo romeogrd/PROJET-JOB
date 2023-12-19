46b3db28176603d60b461a5fd8fd5e8903f25222
romeogrd <romeogerard2@hotmail.fr>
2023-12-19 13:30:18 +0100

    last version

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -90,11 +90,7 @@ module.exports.index_get = async (req, res) => {
       const user = res.locals.user;
       console.log('User in index_get:', user);
   
-<<<<<<< HEAD
       if (user) {
-=======
-      if (user && user.jobs && user.jobs.length > 0) {
->>>>>>> parent of 1549a14 (probleme resolu)
         const jobsDetails = await Promise.all(
           user.jobs.map(async (jobId) => {
             try {
@@ -124,6 +120,7 @@ module.exports.index_get = async (req, res) => {
 
 module.exports.logout_get = (req, res) => {
     res.cookie('jwt', '', { maxAge: 1});
+    res.redirect('/login');
 }
 
 module.exports.newjob_get = (req, res) => {
@@ -143,7 +140,6 @@ module.exports.newjob_post = async (req,res) => {
 
         await currentUser.save();
         res.status(201).json({ job: job._id });
-     
 
     }
     catch(err) {
